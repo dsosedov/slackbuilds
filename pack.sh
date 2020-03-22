1152a92dc9979ccb812af3a7803bdcc9e910be44
Dmitrii Sosedov <dsosedov@users.noreply.github.com>
2020-03-21 21:14:42 -0400

    velero - 1.3.1

--- pack.sh
+++ pack.sh
@@ -4,3 +4,4 @@ tar cvzf packages/cf-cli.tar.gz cf-cli/
 tar cvzf packages/fly.tar.gz fly/
 tar cvzf packages/heroku-client.tar.gz heroku-client/
 tar cvzf packages/minikube.tar.gz minikube/
+tar cvzf packages/velero.tar.gz velero/
