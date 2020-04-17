2ed8e224a6793d8178e2e6d07fb509c4345b2a32
Dmitrii Sosedov <dsosedov@users.noreply.github.com>
2020-04-16 20:42:15 -0400

    crystal-lang - 0.34.0

--- pack.sh
+++ pack.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 mkdir -p packages
-tar cvzf packages/cf-cli.tar.gz cf-cli/
-tar cvzf packages/fly.tar.gz fly/
-tar cvzf packages/heroku-client.tar.gz heroku-client/
-tar cvzf packages/minikube.tar.gz minikube/
-tar cvzf packages/velero.tar.gz velero/
+if [ -z $1 ]
+then
+    echo "Missing package"
+else
+    tar cvzf packages/$1.tar.gz $1/
+fi
